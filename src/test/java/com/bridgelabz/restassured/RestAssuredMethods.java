e42f275e042c3abed236cb868ff59b91c597616b
priyankapatil26 <priyankapatil2672000@gmail.com>
2021-11-05 17:02:34 +0530

    get,post,put,delete methods added

---
+++ src/test/java/com/bridgelabz/restassured/RestAssuredMethods.java
@@ -0,0 +1,62 @@
+package com.bridgelabz.restassured;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import static com.sun.webkit.perf.WCGraphicsPerfLogger.log;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
+public class RestAssuredMethods {
+
+    @Test
+    public void getMethod() {
+        given().
+                header("Content-Type", "application/json").
+                get("https://reqres.in/api/users?page=2").
+        then().
+                statusCode(200);
+    }
+
+    @Test
+    public void postMethod(){
+        JSONObject request = new JSONObject();
+        request.put("name", "John");
+        request.put("job","Teacher");
+        System.out.println(request);
+        System.out.println(request.toJSONString());
+        given().
+                header("Content-Type", "application/json").
+                body(request.toJSONString()).
+        when().
+                post("https://reqres.in/api/users").
+        then().
+                statusCode(201);
+    }
+
+    @Test
+    public void putMethod(){
+        JSONObject request = new JSONObject();
+        request.put("name", "John");
+        request.put("job","Teacher");
+        System.out.println(request);
+        System.out.println(request.toJSONString());
+        given().
+                header("Content-Type", "application/json").
+                body(request.toJSONString()).
+        when().
+                put("https://reqres.in/api/users/2").
+        then().
+                statusCode(200);
+    }
+
+    @Test
+    public void deleteMethod(){
+        when().
+                delete("https://reqres.in/api/users/2").
+        then().
+                statusCode(204);
+
+    }
+
+}
